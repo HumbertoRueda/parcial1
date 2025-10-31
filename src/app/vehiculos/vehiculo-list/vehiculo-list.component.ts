32dc075c3588f398b10331ccf6a9d1bba88889a1
Humberto <h.rueda@uniandes.edu.co>
2025-10-31 15:42:23 -0500

    creacion del componente

--- src/app/vehiculos/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculos/vehiculo-list/vehiculo-list.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehiculoService } from '../vehiculo.service';
+import { Vehiculo } from '../vehiculo.model';
 
 @Component({
   selector: 'app-vehiculo-list',
-  standalone: true,
-  imports: [],
   templateUrl: './vehiculo-list.component.html',
-  styleUrl: './vehiculo-list.component.scss'
 })
-export class VehiculoListComponent {
+export class VehiculoListComponent implements OnInit {
+  vehiculos: Vehiculo[] = [];
+  loading = true;
+  error = '';
 
+  constructor(private vehiculoService: VehiculoService) {}
+
+  ngOnInit(): void {
+    this.vehiculoService.getVehiculos().subscribe({
+      next: data => { this.vehiculos = data; this.loading = false; },
+      error: () => { this.error = 'No fue posible cargar los vehículos.'; this.loading = false; }
+    });
+  }
 }
